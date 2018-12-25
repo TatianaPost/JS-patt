5ddcc6ca0315c679fd25f1c62d05e92e75b65dd2
TatianaPost <tpostolova@yahoo.com>
2018-12-25 18:51:27 +0100

    init commit

---
+++ counter.js
@@ -0,0 +1,53 @@
+// Анонимная самовызывающаяся функция
+// Запишем функцию в переменную counter
+var counter = (function(){
+    // Приватные переменные
+    var _number;
+
+    // Приватные методы
+    var _checkPositive = function(x) {
+         if(x < 0){
+	         return false;
+	         }else {
+		         return true;
+	         }
+    }
+
+    // Публичные методы
+    var setValue = function(x){
+        if (_checkInteger(x)) {
+            _number = x;
+        } else {
+            console.log('Неверное значение');
+        }
+    }
+
+    var increaseCounter = function(){
+        _number++;
+    }
+
+    var decreaseCounter = function(){
+        _number--;
+    }
+
+    var printCounter = function(){
+        console.log(_number);
+    }
+
+    // Чтобы иметь доступ к внутренним переменным, их надо вернуть через return
+    return {
+        setValue,
+        increaseCounter,
+        decreaseCounter,
+        printCounter
+    }
+}());
+
+counter.setValue(-3);
+counter.printCounter();
+
+counter.setValue(10);
+counter.increaseCounter();
+counter.printCounter();
+counter.decreaseCounter();
+counter.printCounter();
